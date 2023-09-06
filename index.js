eb62d1250d7bd3383c6d60251bb3bb46d3aba660
changhyun-jang <86968048+changhyun-jang@users.noreply.github.com>
2023-09-06 21:02:36 +0900

    Revert "[Feat] 마이 페이지 및 메인 페이지 프론트엔드 구현"

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+const session = require("express-session");
 const app = express();
 const PORT = 8000;
 app.set('view engine', 'ejs');
@@ -7,15 +7,6 @@ app.use('/views', express.static('./views'));
 app.use('/static', express.static('./static'));
 app.set(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get('/', (req, res) => {
-    res.render('main');
-})
-
-app.get('/mypage', (req, res) => {
-    res.render('mypage');
-})
 
 app.listen(PORT, ()=>{
     console.log('local 연결 완료');
