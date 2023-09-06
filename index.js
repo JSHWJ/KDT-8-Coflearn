a2a93bd8943fdfb67fcb749665fcdd3a6d9f0685
JeongHyun <ssa7053@naver.com>
2023-09-06 17:57:08 +0900

    [Refactor] 회원가입/로그인 페이지 front 구현

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const db = require("./models");
 const app = express();
-const PORT = 8000;
+const PORT = 8080;
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
