a399cafb11a2e551e90ae573c1bda31558221b7e
JCH <ckdgusjang@naver.com>
2023-09-12 10:23:25 +0900

    [feat]리코프런 만들기 완료 (#1)

--- controller/C_hi_main.js
+++ controller/C_hi_main.js
@@ -8,20 +8,6 @@ const mypage = (req, res) => {
   res.render("mypage");
 };
 
-// 프로젝트리스트 페이지
-const projectlist = (req, res) => {
-  res.render("projectlist");
-};
-// 리코프런리스트 페이지
-const recoplearnlist = (req, res) => {
-  res.render("recoplearn");
-};
-
-//프로젝트 업로드 페이지
-const project = (req, res) => {
-  res.render("project");
-};
-
 //메인페이지
 const main = (req, res) => {
   res.render("main");
@@ -84,10 +70,8 @@ const signup = (req, res) => {
 const login_modal = (req, res) => {
   res.render("login_modal");
 };
+
 module.exports = {
-  projectlist,
-  recoplearnlist,
-  project,
   main,
   detail,
   login_modal,
